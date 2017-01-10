4593accaa3ac6cd70c0aa289840fe8f54e179a9c
iskraL <iskra.lumbeva@gmail.com>
2017-01-10 14:04:46 +0200

    Login
    Return type to be discussed

--- AvocadoHealthMain/Main/Login.cs
+++ AvocadoHealthMain/Main/Login.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Main
+﻿namespace Main
 {
-    class Login
+    using System.Collections.Generic;
+
+    public class UsernamesAndPasswords
     {
+        private Dictionary<string, string> users;
+        private Dictionary<string, string> trainers;
+
+        public UsernamesAndPasswords()
+        {
+            this.Users = users;
+            this.Users = new Dictionary<string, string>();
+            this.Trainers = trainers;
+            this.Trainers = new Dictionary<string, string>();
+        }
+
+        public Dictionary<string, string> Users { get; private set; }
+        public Dictionary<string, string> Trainers { get; private set; }
+
+        public Dictionary<string, string> AddCredentialsToUsers()
+        {
+            this.Users.Add("Iskra", "Lumbeva");
+            this.Users.Add("Stefan", "Dimitrov");
+            this.Users.Add("Martin", "Kanovsky");
+            return this.Users;
+        }
+
+        public Dictionary<string, string> AddCredentialsToTrainers()
+        {
+            this.Trainers.Add("Milena", "Shishkova");
+            this.Trainers.Add("Vasilena", "Ilieva");
+            this.Trainers.Add("Svetlomir", "Balevski");
+            return this.Trainers;
+        }
     }
 }
