97d1ac4986ac75af8b1bf82794f8d29b66c092b3
iskraL <iskra.lumbeva@gmail.com>
2017-01-10 14:53:19 +0200

    Login changes

--- AvocadoHealthMain/Main/Login.cs
+++ AvocadoHealthMain/Main/Login.cs
@@ -1,37 +1,72 @@
 ﻿namespace Main
 {
+    using Main.Interfaces;
+    using System;
     using System.Collections.Generic;
 
-    public class UsernamesAndPasswords
+
+    class Login : ILogIn
     {
-        private Dictionary<string, string> users;
-        private Dictionary<string, string> trainers;
+        private readonly string username;
+        private readonly string password;
+        private Dictionary<string, string> usersList;
+        private Dictionary<string, string> trainersList;
+        UsernamesAndPasswords obj = new UsernamesAndPasswords();
 
-        public UsernamesAndPasswords()
+        public Login()
         {
-            this.Users = users;
-            this.Users = new Dictionary<string, string>();
-            this.Trainers = trainers;
-            this.Trainers = new Dictionary<string, string>();
+            this.Username = Console.ReadLine();
+            this.Password = Console.ReadLine();
+            this.UsersList = obj.AddCredentialsToUsers();
+            this.TrainersList = obj.AddCredentialsToTrainers();
+            //Asks for credentials until they are valid
+            while (this.UsersList.ContainsKey(this.Username) == false || this.UsersList[this.Username].Contains(this.Password) == false)
+            {
+                Console.WriteLine("Invalid input! Please, try again: ");
+                this.Username = Console.ReadLine();
+                this.Password = Console.ReadLine();
+            }
         }
 
-        public Dictionary<string, string> Users { get; private set; }
-        public Dictionary<string, string> Trainers { get; private set; }
+        public string Username { get; }
+        public string Password { get; }
+        public Dictionary<string, string> UsersList { get; }
+        public Dictionary<string, string> TrainersList { get; }
 
-        public Dictionary<string, string> AddCredentialsToUsers()
+        public Type LogIn()
         {
-            this.Users.Add("Iskra", "Lumbeva");
-            this.Users.Add("Stefan", "Dimitrov");
-            this.Users.Add("Martin", "Kanovsky");
-            return this.Users;
-        }
+            if (this.UsersList.ContainsKey(this.Username))
+            {
+                if (this.UsersList[this.Username].Contains(this.Password))
+                {
+                    return typeof(Trainer);
+                }
 
-        public Dictionary<string, string> AddCredentialsToTrainers()
-        {
-            this.Trainers.Add("Milena", "Shishkova");
-            this.Trainers.Add("Vasilena", "Ilieva");
-            this.Trainers.Add("Svetlomir", "Balevski");
-            return this.Trainers;
+                else
+                {
+                    Console.WriteLine("Wrong password!");
+                    return typeof(Person);
+                }
+            }
+
+            else if (this.TrainersList.ContainsKey(this.Username))
+            {
+                if (this.TrainersList[this.Username].Contains(this.Password))
+                {
+                    return typeof(User);
+                }
+
+                else
+                {
+                    Console.WriteLine("Wrong password!");
+                    return typeof(Person);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You are not registered!");
+                return typeof(Person);
+            }
         }
     }
 }
